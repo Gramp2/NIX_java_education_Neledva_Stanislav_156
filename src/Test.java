181547f3f9208d3c68e483a2bcfd248f5941824b
Stanislav_Neledva <mr.neledva@gmail.com>
2021-10-05 16:10:52 +0300

    Resolved conflict
    Test.java added to feature_branch with change on string #2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Java education");
+        System.out.println("education");
+        //by Grampy
+        System.out.println("DICT");
     }
 }
